e32290c117922a70dac6216c241d78bd3e225f72
Senpaisgirl <rlara13@gmx.de>
2025-08-08 23:47:40 +0200

    first test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskList from './components/tasklist/TaskList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      background: "transparent",
+      display: "flex",
+      justifyContent: "flex-end", // Align to the right
+      padding: "10px",
+    }}>
+      <TaskList />
     </div>
   );
 }
---
+++ src/components/tasklist/TaskList.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import tmi from "tmi.js";
+import "./TaskList.css";
+
+export default function TaskList() {
+
+    return (
+        <div>Hello</div>
+    )
+}
