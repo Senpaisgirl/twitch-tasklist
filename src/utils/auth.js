5119785ade8b86dbc3cb0aa7b878f71ecc14594c
Senpaisgirl <rlara13@gmx.de>
2025-08-10 16:04:48 +0200

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,8 @@
+const API_CALL = "https://9e74ab69-e2c5-4922-8744-e12d7bc1e324-00-3orw17jg8x8ec.spock.replit.dev";
+
 export async function fetchAccessToken() {
   try {
-    const response = await fetch("https://9e74ab69-e2c5-4922-8744-e12d7bc1e324-00-3orw17jg8x8ec.spock.replit.dev/refresh-token");
+    const response = await fetch(API_CALL);
     if (!response.ok) throw new Error("Failed to fetch token");
     const data = await response.json();
     return data.access_token;  // new access token
